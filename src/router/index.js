d5d40375095042059a1ea57f92e16d7f47334410
ldex <laurent.duveau+git@gmail.com>
2021-06-11 16:36:25 -0400

    Fin du jour 1

---
+++ src/router/index.js
@@ -0,0 +1,50 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Products from '@/views/Products.vue';
+import Error from '@/views/Error.vue';
+import ProductDetails from '@/components/ProductDetails.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/products',
+    name: 'products',
+    component: Products
+  },
+  {
+    path: '/product/:id',
+    name: 'product',
+    component: ProductDetails,
+    props: castRouteParamsId
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'error',
+    component: Error
+  }
+]
+
+function castRouteParamsId(route) {
+  return {
+    id: Number(route.params.id),
+  };
+}
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
